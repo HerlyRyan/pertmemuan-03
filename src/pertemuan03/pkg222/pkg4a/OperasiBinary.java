8758d4dea1c703257dd1766784a87acd75b56db4
HerlyRyan <herlyryanth@gmail.com>
2023-05-03 12:51:35 +0800

    Operasi String dan Binary Java

---
+++ src/pertemuan03/pkg222/pkg4a/OperasiBinary.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan03.pkg222.pkg4a;
+
+/**
+ *
+ * @author asus
+ */
+public class OperasiBinary {
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);  
+    }
+}
---
+++ src/pertemuan03/pkg222/pkg4a/OperasiString.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan03.pkg222.pkg4a;
+
+/**
+ *
+ * @author asus
+ */
+public class OperasiString {
+    public static void main(String[] args) {
+        String kota = "Banjarbaru";
+        System.out.println(kota);
+        
+        char[] uniskaChar = {'U', 'N', 'I', 'S', 'K', 'A'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+        
+        System.out.println(kota.toUpperCase());
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.substring(5));
+        System.out.println(kota.substring(0, 4));
+    }
+}
